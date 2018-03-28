d43dd8ea916b1722dfb47de1ed526658e5ab4a96
nickitalebed1 <simple199224@gmil.com>
2018-03-28 11:57:38 +0300

    init cmmit

---
+++ src/main/java/com/jh/lebid/StartUpworkJobHunterApplication.java
@@ -0,0 +1,12 @@
+package com.jh.lebid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUpworkJobHunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartUpworkJobHunterApplication.class, args);
+	}
+}
